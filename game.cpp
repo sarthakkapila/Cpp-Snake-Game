8a90e92b0ec4f7a421070a809f6396c808e91c95
Sarthak Kapila <sarthakkapila27x@gmail.com>
2023-08-27 23:49:09 +0530

    first commit

---
+++ game.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h> // For usleep
+
+const int width = 20;
+const int height = 20;
+
+bool gameOver;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    system("clear"); // Clear the screen (works in Unix-like systems)
+    for (int i = 0; i < width + 2; i++)
+        std::cout << "#";
+    std::cout << std::endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                std::cout << "#";
+            if (i == y && j == x)
+                std::cout << "O";
+            else if (i == fruitY && j == fruitX)
+                std::cout << "F";
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        std::cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    std::cout << " ";
+            }
+
+            if (j == width - 1)
+                std::cout << "#";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        std::cout << "#";
+    std::cout << std::endl;
+    std::cout << "Score:" << score << std::endl;
+}
+
+void Input() {
+    if (std::cin.get() == 'a')
+        dir = LEFT;
+    else if (std::cin.get() == 'd')
+        dir = RIGHT;
+    else if (std::cin.get() == 'w')
+        dir = UP;
+    else if (std::cin.get() == 's')
+        dir = DOWN;
+    else if (std::cin.get() == 'x')
+        gameOver = true;
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        usleep(100000); // Sleep for 100,000 microseconds (0.1 second)
+    }
+    return 0;
+}
